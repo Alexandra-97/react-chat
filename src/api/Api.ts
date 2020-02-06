e4038f934c450b5e9e9b88b07324ab68cb7bd82a
Alexandra-97 <sashabagriy@bk.ru>
2020-02-06 15:30:29 +0200

    add api

---
+++ src/api/Api.ts
@@ -0,0 +1,28 @@
+const me = {
+  id: 0,
+  name: "Alex",
+  avatar: ""
+};
+
+export const users = [
+  {
+    id: 1,
+    name: "Leonid",
+    avatar: "../images/Leonid.jpg"
+  },
+  {
+    id: 2,
+    name: "Scream Smith",
+    avatar: ""
+  },
+  {
+    id: 3,
+    name: "Franz Kafka",
+    avatar: ""
+  },
+  {
+    id: 4,
+    name: "Gregor Zamza",
+    avatar: ""
+  }
+];
